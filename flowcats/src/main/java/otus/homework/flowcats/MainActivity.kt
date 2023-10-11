ff6d6e0c224f22ece7d8473879a46bccd9e1be41
mvrlrd <mvrlrd@yandex.ru>
2023-10-12 00:19:18 +0300

    refactored activity

--- flowcats/src/main/java/otus/homework/flowcats/MainActivity.kt
+++ flowcats/src/main/java/otus/homework/flowcats/MainActivity.kt
@@ -3,6 +3,7 @@ package otus.homework.flowcats
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.lifecycle.Lifecycle
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,8 +15,22 @@ class MainActivity : AppCompatActivity() {
         val view = layoutInflater.inflate(R.layout.activity_main, null) as CatsView
         setContentView(view)
 
-        catsViewModel.catsLiveData.observe(this){
-            view.populate(it)
+        lifecycleScope.launch {
+            lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED){
+                catsViewModel.catsFlow.collect{
+                    when(it){
+                        is Result.Success<Fact> ->{view.populate(it)}
+                        is Result.Error -> {
+                            val text = it.message?:"error"
+                            showToast(text)
+                        }
+                    }
+
+                }
+            }
         }
+    }
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show()
     }
 }
